44402a20cda9abf75b01543c81e2de33903f5ea9
Rainier Barrett <rbarret8@ur.rochester.edu>
2016-11-08 11:58:52 -0500

    Converting to only 1D because this is unmanageable. Passing tests now but need to do more testing for getting Fourier coefficients.

--- pydinger/pydinger.py
+++ pydinger/pydinger.py
@@ -3,33 +3,61 @@ import numpy.polynomial.legendre as L
 
 class Grid:
     '''This class is a grid implementation for holding our input data'''
-    def __init__(self, axes, values):
-        self.axes = axes
-        self.values = values
-        self.dim = len(axes)
+    def __init__(self, axis, values):
+        self.coefficients = []#holds our basis set coefficients later
+        self.axis = np.array(axis)
+        self.values = np.array(values)
         self.c = 1.0#this is the constant used in the Hamiltonian
         self.fourier = True#use Fourier series by default
         self.N = 50#a fairly accurate number
+        self.period = abs(self.axis[0] - self.axis[len(self.axis)-1])#treat as if it's periodic
+        self.offset = self.axis[0]#the offset for plotting to use
 
     def set_c(self, new_c):
+        '''For setting the new constant in the operator.'''
         self.c = new_c
         
     def set_N(self, new_N):
+        '''For setting how big our basis set will be.'''
         self.N = new_N
 
     def set_basis(self, new_bool):
+        '''For choosing which basis set to use.'''
         self.fourier = new_bool
-        
+
+    def get_coefficients(self, func):
+        '''This will dispatch to the appropriate coefficients getter function based on which basis set we're using.'''
+        if(self.fourier == True):
+            self.get_fourier_coefficients( func )
+
+    def cn(self, func, n):
+        '''This calculates the nth Fourier coefficient, using a function represented by a numpy array called 'func'. This is a Riemann sum approximation of the integral we would use, and proves pretty accurate.'''
+        c = func * np.exp(-1j*2*n*np.pi*func/self.period)
+        return(c.sum()/c.size)
+    
+    def f(self, func, x):
+        bounds = np.arange(0.5, self.N + 0.5)
+        coeff_vals = np.array([2*self.cn(func, i)*np.exp(1j*2*i*np.pi*x/self.period) for i in bounds])
+        return(coeff_vals.sum())
+    
+    def get_fourier_coefficients(self, func):
+        '''This fills an array with the N Fourier coefficient values. Only in 1D for now.'''
+        for i in range(self.N):
+            self.coefficients.append(self.cn(func, i))
+
+    def get_fourier_values(self, func):
+        '''This will return a numpy array of values at each point on an axis corresponding to our Fourier coefficients found with get_fourier_coefficients().'''
+        values = np.array([self.f(func, x).real for x in self.axis])
+        return(values)
 
 def read_file(filename, dim = 1):#1D by default
-    coords = [[] for i in range(dim)]#make the empty coordinates 'array'
+    coords = [] 
     values = []
     with open(filename) as f:
         for line in f:
             words = line.split(' ')
-            for i in range(dim):
-                coords[i].append(float(words[i]))
-            values.append(float(words[dim]))#this makes a big long potentials list for higher dims
+            coords.append(float(words[0]))
+            values.append(float(words[1]))#only doing 1D
     return(Grid(coords, values))
 
 def read_input(filename = 'fourier_test_input.txt'):
--- tests/test_pydinger.py
+++ tests/test_pydinger.py
@@ -32,38 +32,20 @@ class TestPydinger(unittest.TestCase):
         '''This tests to make sure we can read in a 1D potential file correctly.'''
         testfile = '1D_test.txt'
         potentials = pydinger.read_file(testfile, 1)
-        assert potentials.dim == 1
-        assert len(potentials.axes[0]) == 200
-        assert potentials.axes[0][0] - 0.000100 < 0.000001
-        assert potentials.axes[0][199] - 1.990000 < 0.000001
-        print(potentials.values[0])
-
-    def test_read_table_2D(self):
-        '''This tests to make sure we can read in a 2D potential file correctly.'''
-        #Do I even need this???
-        testfile = '2D_test.txt'
-        potentials = pydinger.read_file(testfile, 2)
-        assert potentials.dim == 2
-        assert len(potentials.axes[0]) == 121
-        assert potentials.axes[0][0] - 0.000100 < 0.000001
-        assert potentials.axes[0][120] - 2.0001 < 0.000001
-        assert potentials.axes[1][0] - 0.000100 < 0.000001
-        assert potentials.axes[1][120] - 2.0001 < 0.000001
-        assert len(potentials.values) == 121
-        assert potentials.values[0] - 0.00014142135623730951 < 0.000001
-        assert potentials.values[60] - 1.4143549837293323 <  0.000001
-        assert potentials.values[120] - 2.8285685461024275 <  0.000001
+        assert len(potentials.axis) == 200
+        assert potentials.axis[0] - 0.000100 < 0.000001
+        assert potentials.axis[199] - 1.990000 < 0.000001
+        assert potentials.period - 2.0  < 0.000001
+        assert potentials.offset - 0.000100  < 0.000001
 
     def test_read_input(self):
         '''This tests to make sure we can read an input file and fetch the appropriate potential file to go with it.'''
         testfile = 'fourier_test_input.txt'
         potentials = pydinger.read_input(testfile)
         #check that we read the potentials OK
-        assert potentials.dim == 1
-        assert potentials.dim == 1
-        assert len(potentials.axes[0]) == 200
-        assert potentials.axes[0][0] - 0.000100 < 0.000001
-        assert potentials.axes[0][199] - 1.990000 < 0.000001
+        assert len(potentials.axis) == 200
+        assert potentials.axis[0] - 0.000100 < 0.000001
+        assert potentials.axis[199] - 1.990000 < 0.000001
         print(potentials.values[0])
         #check that we have the right values for our constants
         assert potentials.c - 1.0 < 0.000001
@@ -72,14 +54,32 @@ class TestPydinger(unittest.TestCase):
 
         testfile = 'lagrange_test_input.txt'
         potentials = pydinger.read_input(testfile)
-        assert potentials.dim == 1
-        assert potentials.dim == 1
-        assert len(potentials.axes[0]) == 200
-        assert potentials.axes[0][0] - 0.000100 < 0.000001
-        assert potentials.axes[0][199] - 1.990000 < 0.000001
-        print(potentials.values[0])
+        assert len(potentials.axis) == 200
+        assert potentials.axis[0] - 0.000100 < 0.000001
+        assert potentials.axis[199] - 1.990000 < 0.000001
+        print(potentials.values)
         #check that we have the right values for our constants
         assert potentials.c - 1.5 < 0.000001
         assert potentials.N == 10
         assert potentials.fourier == False
+        
+    def test_get_coeffs_fourier(self):
+        '''This tests that we are able to recover a well-behaved function's Fourier polynomial coefficients. Seeing whether they're 'good' is beyond the scope of unit testing, but I made sure to play around with them in a Jupyter Notebook, and it works well.'''
+        testfile = 'fourier_test_input.txt'
+        potentials = pydinger.read_input(testfile)
+        #this is a nice-looking, well-behaved (easy to converge upon) function for a fourier series
+        test_wavefunc = potentials.axis*2 + potentials.axis**2 - potentials.axis**3
+        print("TEST_WAVEFUNCTION IS {}".format(test_wavefunc))
+        potentials.get_coefficients(test_wavefunc)#should calculate the fourier coefficients
+        assert len(potentials.coefficients) == potentials.N
+        values = potentials.get_fourier_values(test_wavefunc)
+        print("VALUES IS {}".format(values))
+        diffsquare = 0
+        #print(potentials.axis[0])
+        for i in range(len(values)):
+            diffsquare +=(values[i] - test_wavefunc[i])**2
+            print("values[{}] is {}".format(i, values[i]))
+            print("and test_wavefunc[{}] is {}".format(i,test_wavefunc[i]))
+#        assert(diffsquare < 10.0)
+
         
